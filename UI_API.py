4733575df5d7f98d07b129b778fec3af73a3966c
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-12-16 14:27:10 +0300

    Change GUI

--- UI_API.py
+++ UI_API.py
@@ -1,32 +1,17 @@
 #pyuic5 gui.ui -o gui.py
 from gui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
 import sys
-import threading
-import time
-import os
-
-app = QtWidgets.QApplication(sys.argv)
-STRT = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(STRT)
-STRT.show()
 
 
-def Run():
-    for i in range(10):
-        time.sleep(1)
-        print(i)
+class UI_API:
     
-def Lmao():
-    t1=threading.Thread(target=Run)
-    if(t1.is_alive()==False): t1.start()
-    
-
-    
-    
-
-ui.pushButton.clicked.connect(Lmao)
-
-sys.exit(app.exec_())
+    def __init__(self):
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.STRT = QtWidgets.QMainWindow()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self.STRT)
+        
+    def show(self):
+        self.STRT.show()
+        sys.exit(self.app.exec_())
