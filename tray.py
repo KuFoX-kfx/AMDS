386f4f826c56d84230c3d4e19138765650d1832a
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-12-16 14:27:25 +0300

    Add some logic to tray

---
+++ tray.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from PyQt5 import QtWidgets, QtGui
+
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+
+    def __init__(self, icon, parent=None):
+        QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
+        self.setToolTip('Animate my discord status')
+        menu = QtWidgets.QMenu(parent)
+        open_app = menu.addAction("Open Notepad")
+        open_app.triggered.connect(self.open_notepad)
+        open_app.setIcon(QtGui.QIcon("icon.png"))
+
+        open_cal = menu.addAction("Open Calculator")
+        open_cal.triggered.connect(self.open_calc)
+        open_cal.setIcon(QtGui.QIcon("icon.png"))
+
+        exit_ = menu.addAction("Exit")
+        exit_.triggered.connect(lambda: sys.exit())
+        exit_.setIcon(QtGui.QIcon("icon.png"))
+
+        menu.addSeparator()
+        self.setContextMenu(menu)
+        self.activated.connect(self.onTrayIconActivated)
+
+    def onTrayIconActivated(self, reason):
+        """
+        This function will trigger function on click or double click
+        :param reason:
+        :return:
+        """
+        if reason == self.DoubleClick:
+            self.open_notepad()
+        # if reason == self.Trigger:
+        #     self.open_notepad()
+
+    def open_notepad(self):
+        """
+        this function will open application
+        :return:
+        """
+        os.system('notepad')
+
+    def open_calc(self):
+        """
+        this function will open application
+        :return:
+        """
+        os.system('calc')
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    tray_icon = SystemTrayIcon(QtGui.QIcon("icon.png"), w)
+    tray_icon.show()
+    tray_icon.showMessage('VFX Pipeline', 'Hello "Name of logged in ID')
+    sys.exit(app.exec_())
