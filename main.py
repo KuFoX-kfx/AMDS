e7033762f474ef3ed08e0bd30cd54ad4471e75cd
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-12-16 14:27:57 +0300

    Add some threading and logic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #Import 
 import Discord_API
+import tray
+import UI_API
 from API import D_API
 import json
+import threading
 import time
 
 
@@ -9,9 +12,27 @@ import time
 
 
 with open("AMDS-kfx.json", "r", encoding="utf8") as json_file: data = json.load(json_file)
-DAPI = Discord_API.Discord_API(data["authToken"])
 
-for i in range(len(data["animation"])):
-    time.sleep(data["timeout"]/1000)
-    DAPI.set_cstatus(text=data["animation"][i]["text"], emoji=data["animation"][i]["emoji_name"])
+def import_ui():
+    UI = UI_API.UI_API()
+    UI.show()
+    
+tray.main()
+
+def set_status():
+    DAPI = Discord_API.Discord_API(data["authToken"])
+    for i in range(len(data["animation"])):
+        time.sleep(data["timeout"]/1000)
+        DAPI.set_cstatus(text=data["animation"][i]["text"], emoji=data["animation"][i]["emoji_name"])
+    
+    
+#t1 = threading.Thread(target=import_ui)
+#t1.start()
+#
+#t2 = threading.Thread(target=set_status)
+#t2.start()
+
+
+
+
     
