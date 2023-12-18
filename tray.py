b2967b3a22a6f3a99356082aa985ca4585425f9e
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-12-18 20:09:11 +0300

    Update tray.py

--- tray.py
+++ tray.py
@@ -1,54 +1,40 @@
-import os
 import sys
+import threading
 from PyQt5 import QtWidgets, QtGui
 
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
 
     def __init__(self, icon, parent=None):
+        
         QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
         self.setToolTip('Animate my discord status')
         menu = QtWidgets.QMenu(parent)
-        open_app = menu.addAction("Open Notepad")
-        open_app.triggered.connect(self.open_notepad)
-        open_app.setIcon(QtGui.QIcon("icon.png"))
+        menu.addAction("AMDS By KuFoX").setEnabled(False)
+        
+        menu.addSeparator()
 
-        open_cal = menu.addAction("Open Calculator")
-        open_cal.triggered.connect(self.open_calc)
-        open_cal.setIcon(QtGui.QIcon("icon.png"))
-
-        exit_ = menu.addAction("Exit")
-        exit_.triggered.connect(lambda: sys.exit())
-        exit_.setIcon(QtGui.QIcon("icon.png"))
+        settings = menu.addAction("Open Settings")
+        settings.triggered.connect(self.activate_ui)
+        settings.setIcon(QtGui.QIcon("icon.png"))
 
         menu.addSeparator()
         self.setContextMenu(menu)
         self.activated.connect(self.onTrayIconActivated)
 
+    def activate_ui(self):
+        from main import import_ui
+        threading.Thread(target=lambda: import_ui()).start()
+        
+    
     def onTrayIconActivated(self, reason):
         """
         This function will trigger function on click or double click
         :param reason:
         :return:
         """
-        if reason == self.DoubleClick:
-            self.open_notepad()
         # if reason == self.Trigger:
         #     self.open_notepad()
-
-    def open_notepad(self):
-        """
-        this function will open application
-        :return:
-        """
-        os.system('notepad')
-
-    def open_calc(self):
-        """
-        this function will open application
-        :return:
-        """
-        os.system('calc')
 
 
 def main():
