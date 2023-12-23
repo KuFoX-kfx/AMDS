26dfee4327cfe63208e287a4f8ec4bd0d156cced
_KuFoX_ <fox3fox2fox1@gmail.com>
2023-12-23 04:45:27 +0300

    try change tray to him file

--- main.py
+++ main.py
@@ -12,36 +12,27 @@ import time
 
 #Import tray class
 def start_animate():
-    sas.start()
-    global event
-    event = True
-    tray_icon.start.setEnabled(False)
-    tray_icon.stop.setEnabled(True)
+    tray.SystemTrayIcon.startbtn()
+    tray.SystemTrayIcon.stopbtn()
     
 def stop_animate():
     for i in threading.enumerate():
         if i.name=="sss":
-            global event
-            event = False
-            tray_icon.start.setEnabled(True)
-            tray_icon.stop.setEnabled(False)
+            tray.SystemTrayIcon.start.setEnabled(True)
+            tray.SystemTrayIcon.stop.setEnabled(False)
 
 
 
 
 
 
-
-            
-
-
-def start_as():
-    with open("AMDS-kfx.json", "r", encoding="utf8") as json_file: data = json.load(json_file)
-    DAPI = Discord_API.Discord_API(data["authToken"])
-    while(event):
-        for i in range(len(data["animation"])):
-            DAPI.set_cstatus(text=data["animation"][i]["text"], emoji=data["animation"][i]["emoji_name"])
-            time.sleep(data["timeout"]/1000)
+def AnimateStatus(self):
+        with open("AMDS-kfx.json", "r", encoding="utf8") as json_file: data = json.load(json_file)
+        DAPI = Discord_API.Discord_API(data["authToken"])
+        while(self.event):
+            for i in range(len(data["animation"])):
+                DAPI.set_cstatus(text=data["animation"][i]["text"], emoji=data["animation"][i]["emoji_name"])
+                time.sleep(data["timeout"]/1000)
     
 
 
@@ -49,22 +40,38 @@ def start_as():
 #with open("AMDS-kfx.json", "w") as json_file: json.dump(data, json_file)
 
 
+
+t1 = threading.Thread(target=lambda: tray.start())
+t1.start()
+def test():
+    while(True): 
+        print(tray.getanimate())
+        time.sleep(1)
+t2 = threading.Thread(target=test)
+t2.start()
+
+tray.SystemTrayIcon.
+
+t1.join()
+
+
     
-app = QtWidgets.QApplication(sys.argv)
-w = QtWidgets.QWidget()
-tray_icon = tray.SystemTrayIcon(QtGui.QIcon("icon.png"), w)
-tray_icon.show()
-tray_icon.showMessage('Animate my discord status', 'Start the AMDS')
-tray_icon.start.triggered.connect(start_animate)
-tray_icon.stop.triggered.connect(stop_animate)
+    
+#app = QtWidgets.QApplication(sys.argv)
+#w = QtWidgets.QWidget()
+#tray_icon = tray.SystemTrayIcon(QtGui.QIcon("icon.png"), w)
+#tray_icon.show()
+#tray_icon.showMessage('Animate my discord status', 'Start the AMDS')
+#tray_icon.start.triggered.connect(start_animate)
+#tray_icon.stop.triggered.connect(stop_animate)
+#
+#event = True
+#sas = threading.Thread(target=start_as)
+#sas.setName("sss")
+#
+#tray_icon.start.setEnabled(True)
+#tray_icon.stop.setEnabled(False)
+#
 
-event = True
-sas = threading.Thread(target=start_as)
-sas.setName("sss")
 
-tray_icon.start.setEnabled(True)
-tray_icon.stop.setEnabled(False)
-
-
-
-sys.exit(app.exec_())
+#sys.exit(app.exec_())
